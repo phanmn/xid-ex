4b914e5abecac5e4b2470f564bba95ef3fbbd398
xeon beowulf <thanhxeon2470@gmail.com>
2024-09-26 14:14:04 +0700

    feat: generate_with_timestamp

--- native/xid/src/lib.rs
+++ native/xid/src/lib.rs
@@ -3,4 +3,9 @@ fn generate() -> String {
     xid::new().to_string()
 }
 
-rustler::init!("Elixir.Xid", [generate]);
+#[rustler::nif]
+fn generate_with_timestamp(timestamp: u64) -> String {
+    xid::new_with_timestamp(timestamp).to_string()
+}
+
+rustler::init!("Elixir.Xid", [generate, generate_with_timestamp]);
