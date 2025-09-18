17101f4b93f9fc51e0245ec74a7f805ad6f29b9e
Nhut Phan <thanquan@gmail.com>
2025-09-18 13:20:06 +0700

    update: xid -> xd in dependencies

--- native/xid/src/lib.rs
+++ native/xid/src/lib.rs
@@ -1,11 +1,11 @@
 #[rustler::nif]
 fn generate() -> String {
-    xid::new().to_string()
+    xd::new().to_string()
 }
 
 #[rustler::nif]
 fn generate_with_timestamp(timestamp: u64) -> String {
-    xid::new_with_timestamp(timestamp).to_string()
+    xd::new_with_timestamp(timestamp).to_string()
 }
 
 rustler::init!("Elixir.Xid", [generate, generate_with_timestamp]);
