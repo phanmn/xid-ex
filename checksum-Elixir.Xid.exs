4e8db289d46728bd669273fe59a56fb371460ef9
Nhut Phan <thanquan@gmail.com>
2025-10-09 11:58:53 +0700

    update: include checksum-*

---
+++ checksum-Elixir.Xid.exs
@@ -0,0 +1,5 @@
+%{
+  "libxid_nif-v0.2.0-nif-2.16-aarch64-apple-darwin.so.tar.gz" => "sha256:218fee0b577bb7da7a7c70589885cd2b2b5c5492613ed591b86a6fd617652908",
+  "libxid_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:10a00597528b644e05ec6555f36e5ae024c776a72c4d78ec7a807e7575c79d27",
+  "libxid_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:d26c6c723d79daee0b1d645f14bbcdfcf2883cfe977ceae6465a7ec5a98fc954"
+}
